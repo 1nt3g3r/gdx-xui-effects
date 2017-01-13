756ad80a3c207291944f0575f53e172412eb6f5e
Ivan Melnichuk <melnichuk.cadet@gmail.com>
2017-01-13 07:33:11 +0200

    Added init class

---
+++ src/main/java/ua/com/integer/gdx/xml/ui/effects/XUIEffects.java
@@ -0,0 +1,13 @@
+package ua.com.integer.gdx.xml.ui.effects;
+
+/**
+ * Init point for XUI Effects
+ */
+public class XUIEffects {
+    /**
+     * Registers effect processors
+     */
+    public static void init() {
+        EffectsProcessor.register();
+    }
+}
